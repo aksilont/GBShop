55260ff1f7c7a2f1589451c98644653a03cfe389
aksilont <aksilont@gmail.com>
2021-02-16 11:09:30 +0300

    Practice 2

---
+++ GBShop/App/Entity/LoginResult.swift
@@ -0,0 +1,13 @@
+//
+//  LoginResult.swift
+//  GBShop
+//
+//  Created by Aksilont on 15.02.2021.
+//
+
+import Foundation
+
+struct LoginResult: Codable {
+    let result: Int
+    let user: User
+}
---
+++ GBShop/Core/Network/AbstractRequestFactory.swift
@@ -0,0 +1,25 @@
+//
+//  AbstractRequestFactory.swift
+//  GBShop
+//
+//  Created by Aksilont on 14.02.2021.
+//
+
+import Foundation
+import Alamofire
+
+protocol AbstractRequestFactory {
+    var errorParser: AbstractErrorParser { get }
+    var sessionManager: Session { get }
+    var queue: DispatchQueue { get }
+    
+    @discardableResult
+    func request<T: Decodable>(request: URLRequestConvertible, completionHander: @escaping (AFDataResponse<T>) -> Void) -> DataRequest
+}
+
+extension AbstractRequestFactory {
+    @discardableResult
+    public func request<T: Decodable>(request: URLRequestConvertible, completionHander: @escaping (AFDataResponse<T>) -> Void) -> DataRequest {
+        return sessionManager.request(request).responseCodable(errorParser: errorParser, queue: queue, completionHandler: completionHander)
+    }
+}
