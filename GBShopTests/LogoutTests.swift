311f748d6f794d53f2cb12391c37e506cef77062
aksilont <aksilont@gmail.com>
2021-02-19 23:33:35 +0300

    Practice 3. Added LogoutTests

--- GBShopTests/LogoutTests.swift
+++ GBShopTests/LogoutTests.swift
@@ -13,11 +13,52 @@ class LogoutTests: XCTestCase {
 
     // MARK: - Positive tests
     func testLogout() throws {
+        let baseUrl = try XCTUnwrap(URL(string: "https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/"))
         
+        let configuration = URLSessionConfiguration.default
+        configuration.httpShouldSetCookies = false
+        configuration.headers = .default
+        
+        let session = Session(configuration: configuration)
+        
+        let logout = Logout(errorParser: ErrorParser(), sessionManager: session, baseUrl: baseUrl)
+        let logoutExpectation = expectation(description: "Logout")
+        
+        logout.logout(id: "555") { (response) in
+            switch response.result {
+            case .success(let modelResult):
+                XCTAssertEqual(modelResult.result, 1)
+                logoutExpectation.fulfill()
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+        }
+        
+        wait(for: [logoutExpectation], timeout: 5.0)
     }
     
     // MARK: - Negative tests
     func testFailedLogout() throws {
+        let baseUrl = try XCTUnwrap(URL(string: "https://failUrl"))
         
+        let configuration = URLSessionConfiguration.default
+        configuration.httpShouldSetCookies = false
+        configuration.headers = .default
+        
+        let session = Session(configuration: configuration)
+        
+        let logout = Logout(errorParser: ErrorParser(), sessionManager: session, baseUrl: baseUrl)
+        let logoutExpectation = expectation(description: "Logout")
+        
+        logout.logout(id: "555") { (response) in
+            switch response.result {
+            case .success(let modelResult):
+                XCTFail("Should have a error \(modelResult)")
+            case .failure:
+                logoutExpectation.fulfill()
+            }
+        }
+        
+        wait(for: [logoutExpectation], timeout: 5.0)
     }
 }
